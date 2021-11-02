9e3f089acdfb745b0b4f1686ff92240ef5200ae6
cov <hsco.dev@gmail.com>
2021-11-02 23:25:55 +0900

    1주차 Compose Basics Step 4

--- week 1-Jetpack Compose basics/COV_BasicsCodelap/app/src/main/java/dev/hsco/cov_basicscodelap/MainActivity.kt
+++ week 1-Jetpack Compose basics/COV_BasicsCodelap/app/src/main/java/dev/hsco/cov_basicscodelap/MainActivity.kt
@@ -3,11 +3,14 @@ package dev.hsco.cov_basicscodelap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.hsco.cov_basicscodelap.ui.theme.COV_BasicsCodelapTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,10 +29,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier.padding(24.dp)
+    )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Text preview")
 @Composable
 fun DefaultPreview() {
     COV_BasicsCodelapTheme {

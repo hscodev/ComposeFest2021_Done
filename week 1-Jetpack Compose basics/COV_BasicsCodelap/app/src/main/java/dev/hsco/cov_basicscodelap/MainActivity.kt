facd63d030608e5f6592bc9060a91a8b4ae50ae0
cov <hsco.dev@gmail.com>
2021-11-04 22:18:34 +0900

    1주차 Compose Basics Step 11

--- week 1-Jetpack Compose basics/COV_BasicsCodelap/app/src/main/java/dev/hsco/cov_basicscodelap/MainActivity.kt
+++ week 1-Jetpack Compose basics/COV_BasicsCodelap/app/src/main/java/dev/hsco/cov_basicscodelap/MainActivity.kt
@@ -3,6 +3,9 @@ package dev.hsco.cov_basicscodelap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -57,7 +60,7 @@ fun OnboardingPreview() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnboarding by rememberSaveable{ mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
@@ -77,7 +80,13 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 @Composable
 private fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -87,7 +96,7 @@ private fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
                 Text(text = name)

6f0c3ee02266fc26192446cb96879ba730cba49f
cov <hsco.dev@gmail.com>
2021-11-10 22:07:36 +0900

    2주차 Layouts in Jetpack Compose Step 7

--- week 2-1-Layouts in JetpackCompose/COV_LayoutsInJetpackCompose/app/src/main/java/dev/hsco/cov_layoutsinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/COV_LayoutsInJetpackCompose/app/src/main/java/dev/hsco/cov_layoutsinjetpackcompose/MainActivity.kt
@@ -3,23 +3,16 @@ package dev.hsco.cov_layoutsinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
 import dev.hsco.cov_layoutsinjetpackcompose.ui.theme.COV_LayoutsInJetpackComposeTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             COV_LayoutsInJetpackComposeTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    ImageList()
+                    BodyContent()
                 }
             }
         }
@@ -35,56 +28,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ImageList() {
-    val listSize = 100
-    val scrollState = rememberLazyListState()
-    val coroutineScope = rememberCoroutineScope()
-    Column {
-        Row {
-            Button(onClick = {
-                coroutineScope.launch {
-                    scrollState.animateScrollToItem(0)
-                }
-            }) {
-                Text("Scroll to the top")
-            }
-
-            Button(onClick = {
-                coroutineScope.launch {
-                    scrollState.animateScrollToItem(listSize - 1)
-                }
-            }) {
-                Text("Scroll to the end")
-            }
-        }
-
-        LazyColumn(state = scrollState) {
-            items(100) {
-                ImageListItem(it)
-            }
-        }
+fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
 }
 
 @Composable
-fun ImageListItem(index: Int) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painter = rememberImagePainter(
-                data = "https://developer.android.com/images/brand/Android_Robot.png"
-            ),
-            contentDescription = "Android Logo",
-            modifier = Modifier.size(50.dp)
-        )
-        Spacer(Modifier.width(10.dp))
-        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
     }
 }
+
 
 @Preview
 @Composable
 fun LayoutsCodelabPreview() {
     COV_LayoutsInJetpackComposeTheme {
-        ImageList()
+        BodyContent()
     }
 }

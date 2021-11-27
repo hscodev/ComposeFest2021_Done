0d8bd6f88c99b290e33cc940419bacce3be29369
cov <hsco.dev@gmail.com>
2021-11-27 16:04:17 +0900

    4주차 Testing in Jetpack Compose - Step 6

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,13 +1,11 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.hasContentDescription
-import androidx.compose.ui.test.hasParent
-import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import com.example.compose.rally.ui.components.RallyTopAppBar
+import androidx.compose.ui.test.onNodeWithText
+import com.example.compose.rally.ui.overview.OverviewBody
 import org.junit.Rule
 import org.junit.Test
-import java.util.*
 
 class TopAppBarTest {
 
@@ -15,24 +13,13 @@ class TopAppBarTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun rallyTopAppBarTest_currentLabelExists() {
-        val allScreens = RallyScreen.values().toList()
+    fun overviewScreen_alertsDisplayed() {
         composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = { },
-                currentScreen = RallyScreen.Accounts
-            )
+            OverviewBody()
         }
 
         composeTestRule
-            .onNode(
-                hasText(RallyScreen.Accounts.name.uppercase(Locale.getDefault())) and
-                        hasParent(
-                            hasContentDescription(RallyScreen.Accounts.name)
-                        ),
-                useUnmergedTree = true
-            )
-            .assertExists()
+            .onNodeWithText("Alerts")
+            .assertIsDisplayed()
     }
 }
